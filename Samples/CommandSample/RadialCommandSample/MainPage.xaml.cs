39a4f6a02e64a488d705e8c45c04a85b5d36f543
Paul Anderson <42271912+PaulAnderson02@users.noreply.github.com>
2020-12-10 19:55:13 +0530

    Sample for Command added

---
+++ Samples/CommandSample/RadialCommandSample/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Syncfusion.SfRadialMenu.XForms;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace RadialCommandSample
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        ViewModel viewModel;
+        public MainPage()
+        {
+            InitializeComponent();
+            viewModel= new ViewModel(this);
+            this.BindingContext = viewModel;
+            //SfRadialMenu radialMenu = new SfRadialMenu();
+            //ObservableCollection<SfRadialMenuItem> itemCollection = new ObservableCollection<SfRadialMenuItem>();
+            //itemCollection.Add(new SfRadialMenuItem() { Text = "Bold", FontSize = 12 , Command = viewModel.Item1Command , CommandParameter = "Bold is tapped"});
+            //itemCollection.Add(new SfRadialMenuItem() { Text = "Copy", FontSize = 12 , Command =viewModel.Item2Command, CommandParameter = "Copy is tapped" });
+            //itemCollection.Add(new SfRadialMenuItem() { Text = "Paste", FontSize = 12, Command = viewModel.Item3Command , CommandParameter = "Paste is tapped" });
+            //itemCollection.Add(new SfRadialMenuItem() { Text = "Undo", FontSize = 12, Command = viewModel.Item4Command, CommandParameter = "Undo is tapped" });
+            //itemCollection.Add(new SfRadialMenuItem() { Text = "Color", FontSize = 12, Command = viewModel.Item5Command, CommandParameter = "Color is tapped" });
+            //radialMenu.Items = itemCollection;
+            //this.Content = radialMenu;
+        }
+    }
+}
---
+++ Samples/CommandSample/RadialCommandSample/ViewModel.cs
@@ -0,0 +1,97 @@
+﻿using Syncfusion.SfRadialMenu.XForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace RadialCommandSample
+{
+    public  class ViewModel : INotifyPropertyChanged
+    { 
+        private ICommand item1Command;
+        public ICommand Item1Command
+        {
+            get { return item1Command; }
+            set
+            {
+                item1Command = value;
+                OnPropertyRaised("Item1Command");
+            }
+        }
+
+        private ICommand item2Command;
+        public ICommand Item2Command
+        {
+            get { return item2Command; }
+            set
+            {
+                item2Command = value;
+                OnPropertyRaised("Item2Command");
+            }
+        }
+
+        private ICommand item3Command;
+        public ICommand Item3Command
+        {
+            get { return item3Command; }
+            set
+            {
+                item3Command = value;
+                OnPropertyRaised("Item3Command");
+            }
+        }
+
+        private ICommand item4Command;
+        public ICommand Item4Command
+        {
+            get { return item4Command; }
+            set
+            {
+                item4Command = value;
+                OnPropertyRaised("Item4Command");
+            }
+        }
+
+        private ICommand item5Command;
+        public ICommand Item5Command
+        {
+            get { return item5Command; }
+            set
+            {
+                item5Command = value;
+                OnPropertyRaised("Item5Command");
+            }
+        }
+
+        Page page;
+
+        public ViewModel(Page page)
+        {
+            this.page = page;
+            Item1Command = new Command(ButtonClicked);
+            Item2Command = new Command(ButtonClicked);
+            Item3Command = new Command(ButtonClicked);
+            Item4Command = new Command(ButtonClicked);
+            Item5Command = new Command(ButtonClicked);
+        }
+
+        private async void ButtonClicked(object sender)
+        {
+            var text = (sender as string).ToString();
+            await page.DisplayAlert("Alert", text, "OK");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyRaised(string propertyname)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyname));
+            }
+
+        }
+    }
+}
